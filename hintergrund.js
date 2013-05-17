131d76d4b29180cda282c85ff680372a41fb6367
Robert Hofer <hofrob@gmail.com>
2013-05-17 10:44:37 +0200

    settimeout im hintergrund endlich hinbekommen

--- hintergrund.js
+++ hintergrund.js
@@ -39,15 +39,87 @@ function open_options() {
 }
 
 function check_preisagenten() {
-	chrome.storage.sync.get('preisagenten', function(syncStorage) {
+	chrome.storage.sync.get(null, function(syncStorage) {
 
 		var preisagenten = syncStorage['preisagenten'];
+		var tabs = syncStorage['tabs'];
 
 		if(!$.isEmptyObject(preisagenten)) {
-			var delay_faktor = 0;
-			for(i in preisagenten) {
-				chrome.alarms.create('preisagent_check_' + i, {'delayInMinutes': delay_faktor / 30});
-				delay_faktor++;
+			for(var i in preisagenten) {
+				(function(i) {
+					setTimeout(function() {
+
+						var tab_id = i.replace(/^.*_(\d+)$/, '$1');
+						var data = getvars_fuer_tab(tabs[tab_id]);
+
+						var region = i.replace(/^(\w{2})_.*$/, '$1');
+						var artikel = i.replace(/^.*_(\d+)_.*$/, '$1');
+						var url = 'http://geizhals.' + region + '/' + artikel;
+
+						$.ajax({
+							url: url,
+							data: data,
+							dataType: 'html',
+							cache: false,
+							dataFilter: function(data) {
+								return data.replace(/<img\b[^>]*>/ig, '');
+							},
+							success: function(data) {
+
+								var preis_span = $('#content_table span.price:first', data);
+								var preis, haendler;
+
+								if(preis_span.length) {
+									preis = parseInt($('#content_table span.price:first', data).text().replace(/,/, '').replace(/\-\-/, '00'), 10);
+									haendler = $('#content_table tr.t1:first td:nth-child(2) a:first', data).text();
+								} else {
+									preis = '--';
+									haendler = '--';
+								}
+
+								chrome.storage.sync.get('preisagenten', function(syncStorage) {
+									var preisagenten = syncStorage['preisagenten'];
+									preisagenten[i].preis = preis;
+									preisagenten[i].haendler = haendler;
+									preisagenten[i].uhrzeit = Date.now();
+									chrome.storage.sync.set({'preisagenten': preisagenten});
+								});
+
+								if(preis != preisagenten[i].preis) {
+
+									var text;
+									if(typeof preis != 'number') {
+										text = 'Kein Preis mehr gefunden im Tab "' + tabs[tab_id].tabname + '".\n\
+											Vorher: € ' + preisagenten[i].preis/100 + ' (' + preisagenten[i].haendler + ')';
+									} else if(typeof preisagenten[i].preis != 'number') {
+										text = 'Preis gefunden im Tab "' + tabs[tab_id].tabname + '": € ' + preis/100 + ' (' + haendler + ')';
+									} else {
+										var differenz = Math.abs((preisagenten[i].preis - preis))/100;
+										var aenderung;
+
+										if(preis < preisagenten[i].preis)
+											aenderung = ' gesunken';
+										else
+											aenderung = ' gestiegen';
+
+										text = 'Preis ist im Tab "' + tabs[tab_id].tabname + '" um € ' + differenz + aenderung + '. \n\
+											Vorher: € ' + preisagenten[i].preis/100 + ' (' + preisagenten[i].haendler + ') \n\
+											Nachher: € ' + preis/100 + ' (' + haendler + ')';
+									}
+
+									notify({
+										'typ': 'notification',
+										'icon': 'preisagent_32.png',
+										'titel': 'Preisagent für "' + preisagenten[i].titel + '"',
+										'text': text,
+										'link': url,
+										'tab_id': tab_id
+									});
+								}
+							}
+						});
+					}, Math.floor(Math.random()*60*1000));
+				}(i));
 			}
 		}
 	});
@@ -56,84 +128,6 @@ function check_preisagenten() {
 chrome.alarms.onAlarm.addListener(function(alarm) {
 	if(alarm.name == 'preisagent_check') {
 		check_preisagenten();
-	} else if(alarm.name.substring(0, 17) == 'preisagent_check_') {
-
-		chrome.storage.sync.get(null, function(syncStorage) {
-
-			var preisagenten = syncStorage['preisagenten'];
-			var tabs = syncStorage['tabs'];
-
-			var i = alarm.name.substring(17);
-			var tab_id = i.replace(/^.*_(\d+)$/, '$1');
-			var data = getvars_fuer_tab(tabs[tab_id]);
-
-			var region = i.replace(/^(\w{2})_.*$/, '$1');
-			var artikel = i.replace(/^.*_(\d+)_.*$/, '$1');
-			var url = 'http://geizhals.' + region + '/' + artikel;
-
-			$.ajax({
-				url: url,
-				data: data,
-				dataType: 'html',
-				cache: false,
-				dataFilter: function(data) {
-					return data.replace(/<img\b[^>]*>/ig, '');
-				},
-				success: function(data) {
-
-					var preis_span = $('#content_table span.price:first', data);
-					var preis, haendler;
-
-					if(preis_span.length) {
-						preis = parseInt($('#content_table span.price:first', data).text().replace(/,/, '').replace(/\-\-/, '00'), 10);
-						haendler = $('#content_table tr.t1:first td:nth-child(2) a:first', data).text();
-					} else {
-						preis = '--';
-						haendler = '--';
-					}
-
-					chrome.storage.sync.get('preisagenten', function(syncStorage) {
-						var preisagenten = syncStorage['preisagenten'];
-						preisagenten[i].preis = preis;
-						preisagenten[i].haendler = haendler;
-						preisagenten[i].uhrzeit = Date.now();
-						chrome.storage.sync.set({'preisagenten': preisagenten});
-					});
-
-					if(preis != preisagenten[i].preis) {
-
-						var text;
-						if(typeof preis != 'number') {
-							text = 'Kein Preis mehr gefunden im Tab "' + tabs[tab_id].tabname + '".\n\
-								Vorher: € ' + preisagenten[i].preis/100 + ' (' + preisagenten[i].haendler + ')';
-						} else if(typeof preisagenten[i].preis != 'number') {
-							text = 'Preis gefunden im Tab "' + tabs[tab_id].tabname + '": € ' + preis/100 + ' (' + haendler + ')';
-						} else {
-							var differenz = Math.abs((preisagenten[i].preis - preis))/100;
-							var aenderung;
-
-							if(preis < preisagenten[i].preis)
-								aenderung = ' gesunken';
-							else
-								aenderung = ' gestiegen';
-
-							text = 'Preis ist im Tab "' + tabs[tab_id].tabname + '" um € ' + differenz + aenderung + '. \n\
-								Vorher: € ' + preisagenten[i].preis/100 + ' (' + preisagenten[i].haendler + ') \n\
-								Nachher: € ' + preis/100 + ' (' + haendler + ')';
-						}
-
-						notify({
-							'typ': 'notification',
-							'icon': 'preisagent_32.png',
-							'titel': 'Preisagent für "' + preisagenten[i].titel + '"',
-							'text': text,
-							'link': url,
-							'tab_id': tab_id
-						});
-					}
-				}
-			});
-		});
 	}
 });
 
