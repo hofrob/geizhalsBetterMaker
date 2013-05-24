b1bf62564f07c9b82ae1a88d9209df2bb5482735
Robert Hofer <hofrob@gmail.com>
2013-05-24 20:07:36 +0200

    seiten nachladen

--- main.js
+++ main.js
@@ -193,3 +193,289 @@ function zeilen_einausblenden() {
 		});
 	});
 }
+
+function content_table_bearbeiten(tab_settings, content_table_rows) {
+
+	if(tab_settings.spaltenueberschriften_ausblenden)
+		$('tr.tr1').remove();
+
+	if(tab_settings.vkinfo_ausblenden && tab_settings.lagerstand_kuerzen) {
+		$('td:nth-child(4)', content_table_rows).each(function(i, value) {
+
+			if($(value).attr('colspan') == 5)
+				return;
+
+			tooltip_anhaengen(value, {richtung:'e'}, function(value) {
+				value.find('div.vk_inl').remove();
+				var lagerstand = value.find('.av_inl');
+
+				if(lagerstand.hasClass('av_l'))
+					lagerstand.html('lagernd');
+				else if(lagerstand.hasClass('av_k'))
+					lagerstand.html('bis 4 Werktage');
+				else
+					lagerstand.html('4+ Werktage');
+
+				return value.html();
+			});
+		});
+	} else if(tab_settings.vkinfo_ausblenden) {
+		$('div.vk_inl', content_table_rows).each(function(i, value) {
+			tooltip_anhaengen(value, {richtung:'e'}, function() {
+				return 'Versandkosten';
+			});
+		});
+	} else if(tab_settings.lagerstand_kuerzen) {
+		$('div.av_l', content_table_rows).each(function(i, value) {
+			tooltip_anhaengen(value, {richtung:'e'}, function() {
+				return 'lagernd';
+			});
+		});
+		$('div.av_k', content_table_rows).each(function(i, value) {
+			tooltip_anhaengen(value, {richtung:'e'}, function() {
+				return 'bis 4 Werktage';
+			});
+		});
+		$('div.av_e', content_table_rows).each(function(i, value) {
+			tooltip_anhaengen(value, {richtung:'e'}, function() {
+				return '4+ Werktage';
+			});
+		});
+	}
+
+
+	if(tab_settings.preisfeld_ausmisten && tab_settings.beschreibungstext_kuerzen)
+		var preis_von = [];
+
+	if(tab_settings.beschreibungstext_kuerzen)
+		$('.ty2', content_table_rows).each(function(i, value) {
+			tooltip_anhaengen(value, {richtung:'s'}, function(value) {
+
+				var beschreibung_haendler = value.html().split("<p>")[0]
+						.replace(/\<br\>|\<wbr\>/g, ' ')
+						.substring(0,250);
+
+				if(tab_settings.preis_vom_zeitdifferenz) {
+					var datum = value.find('p b').text(),
+						alter = errechne_alter(datum),
+						small = $(document.createElement('small'))
+							.html(alter)
+							.attr('title', datum)
+							.addClass('ghbm_zeit');
+
+					preis_von.push(small[0].outerHTML);
+
+					return beschreibung_haendler;
+				} else {
+					return beschreibung_haendler + '<br>' + small[0].outerHTML;
+				}
+			});
+		});
+
+	if(tab_settings.preisfeld_ausmisten)
+		$('td:nth-child(1)', content_table_rows).each(function(i, value) {
+
+			if($(value).attr('colspan') == 5)
+				return;
+
+			tooltip_anhaengen(value, {richtung:'w'}, function(value) {
+
+				var preis = value.find('span.price').clone(),
+					kkimg;
+
+				if(!tab_settings.kreditkartenlogos_ausblenden)
+					kkimg = value.find('p').last().clone();
+
+				value.empty();
+
+				var stand = '';
+				if(tab_settings.preis_vom_zeitdifferenz)
+					stand = preis_von.shift();
+
+				if(typeof kkimg === 'undefined' || kkimg.length == 0 || /MwSt/.test(kkimg[0].innerText))
+					return preis[0].outerHTML + '<br>' + stand;
+				else
+					return preis[0].outerHTML + '<br>' + stand + ' ' + kkimg[0].outerHTML;
+			});
+		});
+
+	if(tab_settings.bewertungsinfo_kuerzen)
+		$('td:nth-child(3)', content_table_rows).each(function(i, value) {
+
+			if($(value).attr('colspan') == 5)
+				return;
+
+			tooltip_anhaengen(value, {richtung:'e'}, function(value) {
+
+				var div_helper = $(document.createElement('div'));
+
+				var a = value.find('a').first().clone();
+				if(/hat keine g.ltigen bewertungen/i.test(value.text())) {
+					a.html('keine Bewertungen');
+				} else {
+					var bewertungen = value.find('small a').text().replace(/.*?(\d+)\s*Bewertung(en)?/i, '$1'),
+						note = value.find('small:first').text().replace(/Note.*?([\d,]+).*/i, '$1');
+					note = parseFloat(note.replace(/,/, '.')).toFixed(2).replace(/\./, ',');
+					a.append('<br>' + note + '/' + bewertungen);
+				}
+
+				div_helper.append(a);
+
+				if($('span.gh_stars', value).length)
+					div_helper.append('<br>amazon: ', $('span.gh_stars', value));
+
+				return div_helper.html();
+			});
+		});
+
+	if(tab_settings.haendlerlink_kuerzen) {
+		$('td:nth-child(2)', content_table_rows).each(function(i, value) {
+
+			if($(value).attr('colspan') == 5)
+				return;
+
+			var processTooltip = function(div) {
+
+				if($('.haendler_ausblenden', div).length)
+					return div;
+
+				var a = $(document.createElement('a'))
+						.html('GHBM Optionen')
+						.attr({
+							href: '#',
+							onClick: 'return false;'
+						})
+						.click(function() {
+							$('#powerTip .haendler_ausblenden').show();
+						});
+
+				$('.gh_hl1', div).append(' ', a);
+
+				var div_haendler_ausblenden = $(document.createElement('div')),
+					img_temp_herv = $(document.createElement('img'))
+						.attr('src', chrome.extension.getURL('img/hinzufuegen.png'))
+						.addClass('haendler_bearbeiten_icon')
+						.attr('data-ausblendart', 't_1'),
+					img_perm_herv = img_temp_herv.clone().attr('data-ausblendart', 'p_1'),
+					img_temp_haen = $(document.createElement('img'))
+						.attr('src', chrome.extension.getURL('img/entfernen.png'))
+						.addClass('haendler_bearbeiten_icon')
+						.attr('data-ausblendart', 't_2'),
+					img_perm_haen = img_temp_haen.clone().attr('data-ausblendart', 'p_2'),
+					img_temp_region = img_temp_haen.clone().attr('data-ausblendart', 't_3'),
+					img_perm_region = img_temp_haen.clone().attr('data-ausblendart', 'p_3');
+
+				div_haendler_ausblenden
+						.addClass('haendler_ausblenden')
+						.append('<br>Händler hervorheben:<br>',
+								img_temp_herv, ' temporär (4h)<br>',
+								img_perm_herv, ' permanent<br>',
+								'<br>Händler ausblenden:<br>',
+								img_temp_haen, ' temporär (4h)<br>',
+								img_perm_haen, ' permanent<br>',
+								'<br>Region ', $('img:first', value).clone(), ' ausblenden:<br>',
+								img_temp_region, ' temporär (4h)<br>',
+								img_perm_region, ' permanent');
+
+				$('.haendler_bearbeiten_icon', div_haendler_ausblenden).click(function(e) {
+
+					$.powerTip.hide();
+
+					var haendlername = $('#powerTip a:first').text();
+
+					chrome.storage.sync.get('haendler', function(syncStorage) {
+
+						var haendler = syncStorage['haendler'],
+							art = $(e.target).attr('data-ausblendart').split('_'),
+							temp = art[0] == 't',
+							typ = parseInt(art[1], 10),
+							name = art[1] == 3 ? get_region($('img:first', value).attr('src')) : haendlername,
+							eintrag = $.grep(haendler, function(e) {
+								return e.name == name && (typ == 3 && e.typ == 3 || typ < 3 && e.typ < 3);
+							});
+
+						if(eintrag.length) {
+							eintrag[0].temp = temp;
+							eintrag[0].typ = typ;
+							eintrag[0].zeit = Date.now();
+							haendler = $.grep(haendler, function(e) {
+								return e.name == name && (typ == 3 && e.typ == 3 || typ < 3 && e.typ < 3);
+							}, true);
+							haendler.push(eintrag[0]);
+						} else {
+							haendler.push({
+								name: name,
+								temp: temp,
+								typ: typ,
+								zeit: Date.now()
+							});
+						}
+
+						chrome.storage.sync.set({haendler: haendler}, function() {
+							chrome.runtime.sendMessage({typ: 'zeilen_einausblenden'});
+						});
+					});
+				});
+
+				div.append(div_haendler_ausblenden);
+				return div;
+			};
+
+			var alte_flag = $(value).find('img.hlflg');
+
+			if(alte_flag.length) {
+				var div_haendler_ausblenden = $(document.createElement('div'))
+						.html('Händler aus Region ausblenden'),
+					img_region = $(document.createElement('img'))
+						.attr('src', $(value).find('img.hlflg').attr('src')),
+					div_flag = $(document.createElement('div'))
+						.addClass('powerTip flag_links')
+						.data('powertipjq', div_haendler_ausblenden)
+						.append(img_region);
+
+				alte_flag.remove();
+			}
+
+			tooltip_anhaengen(value, {richtung:'s', processTooltip: processTooltip}, function(value) {
+
+				var haendlerlink_neu = $(document.createElement('div'))
+						.addClass('haendlerlink');
+
+				if(!tab_settings.info_agb_link_ausblenden)
+					haendlerlink_neu.prepend(value.find('.gh_hl1'));
+
+				var link = value.find('a:first').clone().empty(),
+					haendlerlogo = value.find('a:first img');
+
+				if(haendlerlogo.length) {
+					var img_haendlerlogo = $(document.createElement('img'))
+							.attr('src', haendlerlogo.attr('src'));
+
+					link.append(img_haendlerlogo, '<br>', value.find('a small'));
+				} else {
+					link.html(value.find('a:first'))
+							.prepend('<br>');
+				}
+				haendlerlink_neu.prepend(link);
+
+				return haendlerlink_neu;
+			});
+			$(value).prepend(div_flag);
+		});
+
+		$('.powerTip_e').powerTip({
+			placement: 'e',
+			mouseOnToPopup: true
+		});
+
+		$('.powerTip_s').powerTip({
+			placement: 's',
+			mouseOnToPopup: true
+		});
+
+		$('.powerTip_w').powerTip({
+			placement: 'w',
+			mouseOnToPopup: true
+		});
+	}
+}
--- tabs.js
+++ tabs.js
@@ -11,11 +11,18 @@ $(function() {
 				.html('BetterMaker Tabs'),
 			span = $(document.createElement('span'))
 				.addClass('monav')
+				.css('font-weight', 'bold')
 				.append(a);
 
 		$('#monav span:first').after(span);
 		return;
 	}
+
+	setInterval(function() {
+		$('.ghbm_zeit').each(function(index, value) {
+			$(value).html(errechne_alter($(value).attr('title')));
+		});
+	}, 60*1000);
 
 	chrome.storage.sync.get(null, function(syncStorage) {
 		var tabs = syncStorage['tabs'],
@@ -95,10 +102,10 @@ $(function() {
 				if($('#preistab_inhalt' + i + ' #content_table').length > 0)
 					return;
 
-				data = getvars_fuer_tab(tabs[i]);
+				var getvars = getvars_fuer_tab(tabs[i]);
 
 				$.ajax({
-					data: data,
+					data: getvars,
 					dataType: 'html',
 					success: function(data) {
 
@@ -111,293 +118,50 @@ $(function() {
 						/* [name="filterbox"] enthaelt script tag mit javascript call der uncaught reference error verursacht
 						 * $('[name="filterbox"]', data).find('script').remove() funktioniert nicht
 						 */
-						$('#gh_afilterbox, #content_table, #gh_content_wrapper > h3, #gh_content_wrapper > div.blaettern', data)
+						$('#gh_afilterbox, #content_table, #gh_content_wrapper > h3', data)
 								.appendTo('#preistab_inhalt' + i);
+
+						if($('span.blaettern:first').length) {
+							var letzte_seite = $('span.blaettern:first a:eq(-2)').text(),
+								button = $(document.createElement('button'))
+									.html('Seite 2/' + letzte_seite + ' laden')
+									.attr({
+										id: 'endloser_geizhals',
+										'data-naechste-seite': '2'
+									})
+									.click(function(e) {
+										$('#endloser_geizhals').hide();
+										getvars.pg = parseInt($(e.target).closest('button').attr('data-naechste-seite'), 10);
+										$.ajax({
+											data: getvars,
+											dataType: 'html',
+											success: function(data) {
+												var neue_zeilen = $('#preistab_inhalt' + i + ' #content_table tr:last').index();
+												$('#preistab_inhalt' + i + ' #content_table tbody').append($('#content_table tbody', data).html());
+												content_table_bearbeiten(tabs[i], $('#preistab_inhalt' + i + ' #content_table tr:gt(' + neue_zeilen + ')'));
+												zeilen_einausblenden();
+
+												if(getvars.pg < parseInt(letzte_seite,10))
+													$('#endloser_geizhals')
+														.attr('data-naechste-seite', (getvars.pg+1).toString())
+														.html('Seite ' + (getvars.pg+1).toString() + '/' + letzte_seite + ' laden')
+														.show();
+												else
+													$('#endloser_geizhals').remove();
+											}
+										});
+									})
+									.button();
+
+							$('#preistab_inhalt' + i + ' #content_table').after('<br>', button);
+						}
+
+						content_table_bearbeiten(tabs[i], $('#preistab_inhalt' + i + ' #content_table tr'));
 
 						zeilen_einausblenden();
 
 						if(tabs[i].filterbox_ausblenden)
 							$('#preistab_inhalt' + i + ' #gh_afilterbox').hide();
-
-						if(tabs[i].vkinfo_ausblenden && tabs[i].lagerstand_kuerzen) {
-							$('#preistab_inhalt' + i + ' #content_table tr td:nth-child(4)').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'e'}, function(value) {
-									value.find('div.vk_inl').remove();
-									var lagerstand = value.find('.av_inl');
-
-									if(lagerstand.hasClass('av_l'))
-										lagerstand.html('lagernd');
-									else if(lagerstand.hasClass('av_k'))
-										lagerstand.html('bis 4 Werktage');
-									else
-										lagerstand.html('4+ Werktage');
-
-									return value.html();
-								});
-							});
-						} else if(tabs[i].vkinfo_ausblenden) {
-							$('#preistab_inhalt' + i + ' div.vk_inl').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'e'}, function() {
-									return 'Versandkosten';
-								});
-							});
-						} else if(tabs[i].lagerstand_kuerzen) {
-							$('#preistab_inhalt' + i + ' #content_table div.av_l').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'e'}, function() {
-									return 'lagernd';
-								});
-							});
-							$('#preistab_inhalt' + i + ' #content_table div.av_k').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'e'}, function() {
-									return 'bis 4 Werktage';
-								});
-							});
-							$('#preistab_inhalt' + i + ' #content_table div.av_e').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'e'}, function() {
-									return '4+ Werktage';
-								});
-							});
-						}
-
-
-						if(tabs[i].preisfeld_ausmisten && tabs[i].beschreibungstext_kuerzen) {
-							var preis_von = [];
-							setInterval(function() {
-								$('.ghbm_zeit').each(function(index, value) {
-									$(value).html(errechne_alter($(value).attr('title')));
-								});
-							}, 60*1000);
-						}
-
-						if(tabs[i].beschreibungstext_kuerzen)
-							$('#preistab_inhalt' + i + ' .ty2').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'s'}, function(value) {
-
-									var beschreibung_haendler = value.html().split("<p>")[0]
-											.replace(/\<br\>|\<wbr\>/g, ' ')
-											.substring(0,250);
-
-									if(tabs[i].preis_vom_zeitdifferenz) {
-										var datum = value.find('p b').text(),
-											alter = errechne_alter(datum),
-											small = $(document.createElement('small'))
-												.html(alter)
-												.attr('title', datum)
-												.addClass('ghbm_zeit');
-
-										preis_von.push(small[0].outerHTML);
-
-										return beschreibung_haendler;
-									} else {
-										return beschreibung_haendler + '<br>' + small[0].outerHTML;
-									}
-								});
-							});
-
-						if(tabs[i].preisfeld_ausmisten)
-							$('#preistab_inhalt' + i + ' #content_table tr td:nth-child(1)').each(function(index, value) {
-								tooltip_anhaengen(value, {richtung:'w'}, function(value) {
-
-									if(value.attr('colspan') == 5)
-										return;
-
-									var preis = value.find('span.price').clone(),
-										kkimg;
-
-									if(!tabs[i].kreditkartenlogos_ausblenden)
-										kkimg = value.find('p').last().clone();
-
-									value.empty();
-
-									var stand = '';
-									if(tabs[i].preis_vom_zeitdifferenz)
-										stand = preis_von.shift();
-
-									if(typeof kkimg === 'undefined' || kkimg.length == 0 || /MwSt/.test(kkimg[0].innerText))
-										return preis[0].outerHTML + '<br>' + stand;
-									else
-										return preis[0].outerHTML + '<br>' + stand + ' ' + kkimg[0].outerHTML;
-								});
-							});
-
-						if(tabs[i].bewertungsinfo_kuerzen)
-							$('#preistab_inhalt' + i + ' #content_table tr td:nth-child(3)').each(function(index, value) {
-
-								tooltip_anhaengen(value, {richtung:'e'}, function(value) {
-
-									div_helper = $(document.createElement('div'));
-
-									if(value.attr('colspan') == 5)
-										return;
-
-									var a = value.find('a').first().clone();
-									if(/hat keine g.ltigen bewertungen/i.test(value.text())) {
-										a.html('keine Bewertungen');
-									} else {
-										var bewertungen = value.find('small a').text().replace(/.*?(\d+)\s*Bewertung(en)?/i, '$1'),
-											note = value.find('small:first').text().replace(/Note.*?([\d,]+).*/i, '$1');
-										note = parseFloat(note.replace(/,/, '.')).toFixed(2).replace(/\./, ',');
-										a.append('<br>' + note + '/' + bewertungen);
-									}
-
-									div_helper.append(a);
-
-									if($('span.gh_stars', value).length)
-										div_helper.append('<br>amazon: ', $('span.gh_stars', value));
-
-									return div_helper.html();
-								});
-							});
-
-						if(tabs[i].haendlerlink_kuerzen)
-							$('#preistab_inhalt' + i + ' #content_table tr td:nth-child(2)').each(function(index, value) {
-
-								var processTooltip = function(div) {
-
-									if($('.haendler_ausblenden', div).length)
-										return div;
-
-									var a = $(document.createElement('a'))
-											.html('GHBM Optionen')
-											.attr({
-												href: '#',
-												onClick: 'return false;'
-											})
-											.click(function() {
-												$('#powerTip .haendler_ausblenden').show();
-											});
-
-									$('.gh_hl1', div).append(' ', a);
-
-									var div_haendler_ausblenden = $(document.createElement('div')),
-										img_temp_herv = $(document.createElement('img'))
-											.attr('src', chrome.extension.getURL('img/hinzufuegen.png'))
-											.addClass('haendler_bearbeiten_icon')
-											.attr('data-ausblendart', 't_1'),
-										img_perm_herv = img_temp_herv.clone().attr('data-ausblendart', 'p_1'),
-										img_temp_haen = $(document.createElement('img'))
-											.attr('src', chrome.extension.getURL('img/entfernen.png'))
-											.addClass('haendler_bearbeiten_icon')
-											.attr('data-ausblendart', 't_2'),
-										img_perm_haen = img_temp_haen.clone().attr('data-ausblendart', 'p_2'),
-										img_temp_region = img_temp_haen.clone().attr('data-ausblendart', 't_3'),
-										img_perm_region = img_temp_haen.clone().attr('data-ausblendart', 'p_3');
-
-									div_haendler_ausblenden
-											.addClass('haendler_ausblenden')
-											.append('<br>Händler hervorheben:<br>',
-													img_temp_herv, ' temporär (4h)<br>',
-													img_perm_herv, ' permanent<br>',
-													'<br>Händler ausblenden:<br>',
-													img_temp_haen, ' temporär (4h)<br>',
-													img_perm_haen, ' permanent<br>',
-													'<br>Region ', $('img:first', value).clone(), ' ausblenden:<br>',
-													img_temp_region, ' temporär (4h)<br>',
-													img_perm_region, ' permanent');
-
-									$('.haendler_bearbeiten_icon', div_haendler_ausblenden).click(function(e) {
-
-										$.powerTip.hide();
-
-										var haendlername = $('#powerTip a:first').text();
-
-										chrome.storage.sync.get('haendler', function(syncStorage) {
-
-											var haendler = syncStorage['haendler'];
-												art = $(e.target).attr('data-ausblendart').split('_'),
-												temp = art[0] == 't',
-												typ = parseInt(art[1], 10),
-												name = art[1] == 3 ? get_region($('img:first', value).attr('src')) : haendlername,
-												eintrag = $.grep(haendler, function(e) {
-													return e.name == name && (typ == 3 && e.typ == 3 || typ < 3 && e.typ < 3);
-												});
-
-											if(eintrag.length) {
-												eintrag[0].temp = temp;
-												eintrag[0].typ = typ;
-												eintrag[0].zeit = Date.now();
-												haendler = $.grep(haendler, function(e) {
-													return e.name == name && (typ == 3 && e.typ == 3 || typ < 3 && e.typ < 3);
-												}, true);
-												haendler.push(eintrag[0]);
-											} else {
-												haendler.push({
-													name: name,
-													temp: temp,
-													typ: typ,
-													zeit: Date.now()
-												});
-											}
-
-											chrome.storage.sync.set({haendler: haendler}, function() {
-												chrome.runtime.sendMessage({typ: 'zeilen_einausblenden'});
-											});
-										});
-									});
-
-									div.append(div_haendler_ausblenden);
-									return div;
-								};
-
-								var alte_flag = $(value).find('img.hlflg');
-
-								if(alte_flag.length) {
-									var div_haendler_ausblenden = $(document.createElement('div'))
-											.html('Händler aus Region ausblenden'),
-										img_region = $(document.createElement('img'))
-											.attr('src', $(value).find('img.hlflg').attr('src')),
-										div_flag = $(document.createElement('div'))
-											.addClass('powerTip flag_links')
-											.data('powertipjq', div_haendler_ausblenden)
-											.append(img_region);
-
-									alte_flag.remove();
-								}
-
-								tooltip_anhaengen(value, {richtung:'s', processTooltip: processTooltip}, function(value) {
-
-									var haendlerlink_neu = $(document.createElement('div'))
-											.addClass('haendlerlink');
-
-									if(!tabs[i].info_agb_link_ausblenden)
-										haendlerlink_neu.prepend(value.find('.gh_hl1'));
-
-									var link = value.find('a:first').clone().empty(),
-										haendlerlogo = value.find('a:first img');
-
-									if(haendlerlogo.length) {
-										var img_haendlerlogo = $(document.createElement('img'))
-												.attr('src', haendlerlogo.attr('src'));
-
-										link.append(img_haendlerlogo, '<br>', value.find('a small'));
-									} else {
-										link.html(value.find('a:first'))
-												.prepend('<br>');
-									}
-									haendlerlink_neu.prepend(link);
-
-									return haendlerlink_neu;
-								});
-								$(value).prepend(div_flag);
-							});
-
-						if(tabs[i].spaltenueberschriften_ausblenden)
-							$('#preistab_inhalt' + i + ' #content_table tr:lt(2)').hide();
-
-						$('.powerTip_e').powerTip({
-							placement: 'e',
-							mouseOnToPopup: true
-						});
-
-						$('.powerTip_s').powerTip({
-							placement: 's',
-							mouseOnToPopup: true
-						});
-
-						$('.powerTip_w').powerTip({
-							placement: 'w',
-							mouseOnToPopup: true
-						});
 					}
 				});
 
